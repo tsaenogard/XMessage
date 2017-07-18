30f48fd21f2ab74f03bebeceb17170240a74da41
“Eric” <tsaenogard@gmail.com>
2017-07-18 11:00:05 +0800

    Initial Commit

---
+++ XMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMessage
+//
+//  Created by Eric Chen on 2017/7/18.
+//  Copyright © 2017年 MasB1ue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
